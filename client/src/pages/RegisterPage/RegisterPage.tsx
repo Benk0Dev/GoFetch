f59a8af156c12c05d25e1705bd000b6de4c25221
RachelM-T <ec23165@qmul.ac.uk>
2025-03-19 11:26:23 +0000

    created a register form where the user can input their username, email address, password and user type

--- client/src/pages/RegisterPage/RegisterPage.tsx
+++ client/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,9 +1,58 @@
 
 function RegisterPage() {
     return (
-        <>
-        
-        </>
+        <form className={styles.registerForm} onSubmit={handleSubmit}>
+            <div className={styles.inputGroup}>
+                <label>First Name</label>
+                <input 
+                    type="text" 
+                    value={Name} 
+                    placeholder="First Name"
+                    onChange={(e) => setName(e.target.value)}
+                    required 
+                />
+            </div>
+
+            <div className={styles.inputGroup}>
+                <label>Last Name</label>
+                <input 
+                    type="text" 
+                    value={Last Name} 
+                    placeholder="Last Name"
+                    onChange={(e) => setName(e.target.value)}
+                    required 
+                />
+            </div>
+
+            <div className={styles.inputGroup}>
+                <label>Email</label>
+                <input 
+                    type="email" 
+                    value={email} 
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)} 
+                    required 
+                />
+             </div>
+
+            <div className={styles.inputGroup}>
+                <label>Password</label>
+                <input 
+                    type="password" 
+                    value={password} 
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)} 
+                    required 
+                />
+            </div>
+
+            <div className={styles.errorContainer}>
+                {error && <p className={styles.error}>{error}</p>}
+            </div>
+
+
+            <button type="submit" className={styles.loginButton} style={{width: "100%"}}>Login</button>
+        </form>
     );
 }
 
