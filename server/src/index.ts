7630423d2aaf232270ede0d0ab36a170df38d7af
AndreyKarm <66835830+AndreyKarm@users.noreply.github.com>
2025-03-22 23:19:41 +0000

    Added graceful shutdown for the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,3 +7,14 @@ import { server, startHttpServer } from './server/httpServer';
 initCache();
 
 const httpServer = startHttpServer();
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+    console.log('Shutting down gracefully...');
+    process.exit();
+});
+
+process.on('SIGTERM', () => {
+    console.log('Shutting down gracefully...');
+    process.exit();
+});
