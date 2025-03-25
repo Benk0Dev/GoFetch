a44a3e7c4dc631ffc0325cd8d8f82af53aa90d43
BKuliesis <bkuliesis@gmail.com>
2025-03-25 05:21:39 +0000

    Added back button to authentication pages

--- client/src/pages/AuthenticationPage/LoginPage.tsx
+++ client/src/pages/AuthenticationPage/LoginPage.tsx
@@ -2,16 +2,20 @@ import LoginForm from "./LoginForm";
 import { useNavigate } from "react-router-dom";
 import styles from "./AuthenticationPage.module.css";
 import "../../global.css";
+import BackButton from "../../components/BackButton";
 
 function LoginPage() {
     const navigate = useNavigate();
 
     return (
-        <div className={styles.authenticationContainer}>
-            <div className={styles.authenticationBox}>
-                <h2>Login</h2>
-                <LoginForm />
-                <p>Don't have an account? <button onClick={() => navigate("/register")} className={styles.alternateLink}>Sign Up</button></p>
+        <div className={`container ${styles.authentication}`}>
+            <div className={styles.authenticationContainer}>
+                <BackButton />
+                <div className={styles.authenticationBox}>
+                    <h2>Login</h2>
+                    <LoginForm />
+                    <p>Don't have an account? <button onClick={() => navigate("/register")} className={styles.alternateLink}>Sign Up</button></p>
+                </div>
             </div>
         </div>
     );
--- client/src/pages/AuthenticationPage/RegisterPage.tsx
+++ client/src/pages/AuthenticationPage/RegisterPage.tsx
@@ -2,16 +2,20 @@ import { useNavigate } from "react-router-dom";
 import styles from "./AuthenticationPage.module.css";
 import "../../global.css";
 import RegisterForm from "./RegisterForm";
+import BackButton from "../../components/BackButton";
 
 function RegisterPage() {
     const navigate = useNavigate();
 
     return (
-        <div className={styles.authenticationContainer}>
-            <div className={styles.authenticationBox}>
-                <h2>Create an Account</h2>
-                <RegisterForm />
-                <p>Already have an account? <button onClick={() => navigate("/login")} className={styles.alternateLink}>Login</button></p>
+        <div className={`container ${styles.authentication}`}>
+            <div className={styles.authenticationContainer}>
+                <BackButton />
+                <div className={styles.authenticationBox}>
+                    <h2>Create an Account</h2>
+                    <RegisterForm />
+                    <p>Already have an account? <button onClick={() => navigate("/login")} className={styles.alternateLink}>Login</button></p>
+                </div>
             </div>
         </div>
     );
