bd90d2c0877c83e97756386c4c819f925ac91d70
BKuliesis <bkuliesis@gmail.com>
2025-03-31 00:48:35 +0100

    Added back clickable minder cards and hover effects

--- client/src/pages/BrowsePage/MinderCard.tsx
+++ client/src/pages/BrowsePage/MinderCard.tsx
@@ -31,63 +31,59 @@ function MinderCard({ minder }: { minder: any }) {
   const services = minder.minderRoleInfo.services.map((service: any) => service.type);
 
   return (
-    <div>
-      <div className={styles["minder-card"]}>
-        <img src={profileImage} alt={minder.name.fname} width="150" />
+    <div className={styles["minder-card"]} onClick={handleViewProfile}>
+      <img src={profileImage} alt={minder.name.fname} width="150" />
 
-        <div className={styles["minder-card-body"]}>
+      <div className={styles["minder-card-body"]}>
 
-          <h4>{minder.name.fname}</h4>
+        <h4>{minder.name.fname}</h4>
 
-          <div className={styles["distance-rating"]}>
-            <div className={styles["metric"] + " " + styles["distance"]}>
-              <MapPin size={18} />
-              <p>0.7 miles away</p>
-            </div>
-            <div className={styles["metric"] + " " + styles["rating"]}>
-              <Star size={18} />
-              <span>{parseFloat(minder.minderRoleInfo.rating.toFixed(1))}</span>
-              <p style={{display: "flex", alignContent: "center", gap: "7px", marginLeft: "2px"}}><strong>•</strong>{minder.minderRoleInfo.reviews.length} {minder.minderRoleInfo.reviews.length === 1 ? "review" : "reviews"}</p>
-            </div>
+        <div className={styles["distance-rating"]}>
+          <div className={styles["metric"] + " " + styles["distance"]}>
+            <MapPin size={18} />
+            <p>0.7 miles away</p>
           </div>
-
-          <div className={styles["services-container"]}>
-            <span>Services:</span>
-            <div className={styles["services"]}>
-              {services.map((service: any, index: number) => (
-                <span key={index} className={styles["service"]}>
-                  {service}
-                </span>
-              ))}
-            </div>
+          <div className={styles["metric"] + " " + styles["rating"]}>
+            <Star size={18} />
+            <span>{parseFloat(minder.minderRoleInfo.rating.toFixed(1))}</span>
+            <p style={{display: "flex", alignContent: "center", gap: "7px", marginLeft: "2px"}}><strong>•</strong>{minder.minderRoleInfo.reviews.length} {minder.minderRoleInfo.reviews.length === 1 ? "review" : "reviews"}</p>
           </div>
-
-          <div className={styles["price-availability"]}>
-            <div className={styles["metric"]}>
-              <span>Price:</span>
-              <p>From £{cheapestPrice}</p>
-            </div>
-            <div className={styles["metric"]}>
-              <span>Availbility:</span>
-              <p>{minder.minderRoleInfo.availability}</p>
-            </div>
-          </div>
-
-          <div className={styles["buttons"]}>
-            {(!user || user.currentRole === Role.OWNER) ? (
-              <>
-                <button className="btn btn-secondary" onClick={handleViewProfile}>View Profile</button>
-                <button className="btn btn-primary" onClick={handleBook}>Book Now</button>
-              </>
-            ) : (
-              <button className="btn btn-primary" onClick={handleViewProfile}>View Profile</button>
-            )}
-          </div>
-
         </div>
-      </div>
 
-      
+        <div className={styles["services-container"]}>
+          <span>Services:</span>
+          <div className={styles["services"]}>
+            {services.map((service: any, index: number) => (
+              <span key={index} className={styles["service"]}>
+                {service}
+              </span>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles["price-availability"]}>
+          <div className={styles["metric"]}>
+            <span>Price:</span>
+            <p>From £{cheapestPrice}</p>
+          </div>
+          <div className={styles["metric"]}>
+            <span>Availbility:</span>
+            <p>{minder.minderRoleInfo.availability}</p>
+          </div>
+        </div>
+
+        <div className={styles["buttons"]}>
+          {(!user || user.currentRole === Role.OWNER) ? (
+            <>
+              <button className="btn btn-secondary" onClick={handleViewProfile}>View Profile</button>
+              <button className="btn btn-primary" onClick={handleBook}>Book Now</button>
+            </>
+          ) : (
+            <button className="btn btn-primary" onClick={handleViewProfile}>View Profile</button>
+          )}
+        </div>
+
+      </div>
     </div>
   );
 }
